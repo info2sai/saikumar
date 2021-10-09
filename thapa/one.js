97529c0953d30955f3f184c9a2e0d281f85e143e
info2sai <inf02sai213@gmail.com>
2021-10-09 13:17:00 +0530

    thapa

---
+++ thapa/one.js
@@ -0,0 +1,33 @@
+// var myPhoneNumber=9876543210;
+// var myName = "thapa technical"
+
+// console.log(isNaN(myPhoneNumber));
+
+// console.log(isNaN(myName));
+
+// if(isNaN(myName)){
+//     console.log("plz enter valid phone no");
+// }
+ 
+//  console.log(NaN===NaN);
+//  console.log(NaN===NaN);
+//  console.log(Number.isNaN(NaN));
+//  console.log(isNaN(Number.NaN));
+//  console.log(isNaN(NaN));
+//  console.log(NaN===NaN);
+// console.log(Number.NaN===NaN)
+
+// console.log(5+20)
+// var x = 5;
+// var y =5;
+// console.log("is both the x and y are equal or not" + x == y);
+// console.log(x==y);
+
+// console.log("is both the x and y are equal :${x ==y}");
+ 
+// console.log(3+3);
+// console.log(10-3);
+// console.log(20/5);
+// console.log(5+6);
+// console.log("Remainder Operator" + 81%8);
+
